ebc2ca9daca99aac58c7b177ebdf537c31b9e665
Ajay kumar verma <ajaykver9@gmail.com>
2023-12-25 01:49:54 +0530

    Decimal <-> binary

---
+++ Decimals.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+int Dec(int b){
+
+int d=0;
+
+int i=0;
+while(b>0){
+int r = b%10;
+d=pow(2,i++)*r+d;
+b=b/10;
+
+}
+
+
+return d;
+
+}
+
+int Bin(int d){
+int bin=0;
+int i=0;
+
+while(d>0){
+ int bit=d%2;
+ bin =bit*pow(10,i++)+bin; 
+ d=d/2;
+}
+
+return bin;
+}
+
+
+int main(){
+ int decimal=0;
+ cout<<"Enter decimal number."<<endl;
+ 
+ cin>>decimal;
+ 
+ cout<<Bin(decimal)<<endl;
+ cout<<"Enter Binary number."<<endl;
+ int binary=0;
+ cin>>binary;
+ cout<<Dec(binary);
+   return 0;
+}
---
+++ Operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+// Operator  
+// 1 Airthmatics , +,-, * , /, %
+// 2 Logic  ||, & 
+// 3 comparision ,>,<,>=,<=
+// 4 Assigning +=,-=,*=,/=,%=,= 
+// 5 biwise | ,& 
+
+
+
+
+ 
+    return 0;
+}
