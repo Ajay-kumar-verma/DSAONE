8e869b6ffa4b95ca405bd4482278661fdc95d513
Ajay kumar verma <ajaykver9@gmail.com>
2023-12-25 02:55:50 +0530

    cond and loop

---
+++ conditional..cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+ int n=0;
+ cin>>n;
+
+ if(n>5){
+    cout<<"N is greater than 5";
+ }
+ else if(5<5){
+    cout<<"N is less than 5";
+ }
+ else{
+    cout<<"N equals to 5";
+ }
+
+    return 0;
+}
---
+++ loop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+// while, do while, for , for-each
+
+
+for(int i=2;i<4;i++)
+  {
+    cout<<i;
+    
+  }
+  int i=0;
+while(i<8)
+{
+    cout<<i++;
+}
+
+i =0;
+do{
+    cout<<"Hello world";
+  i++;
+}while(i<9);
+
+    return 0;
+}
