217ede5fbe5f0be37ada0b122b165987eca5c73f
Ajay kumar verma <ajaykver9@gmail.com>
2023-12-31 02:06:26 +0530

    Variables

---
+++ Variables_Scope.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ // local variable 
+ // Global varibale
+ // instance variable
+ // function variable
+
+
+ int x=20;
+
+ if(true){
+   int x=30;
+    cout<<x<<endl;
+    if(true){
+     int x=40;
+     cout<<x<<endl;
+    }
+  cout<<x<<endl;
+ }
+ cout<<x<<endl;
+
+
+
+
+
+    return 0;
+}
