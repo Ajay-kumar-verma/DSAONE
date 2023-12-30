2a4f586759e049f53f3c7fc4041b96f5a77a8004
Ajay kumar verma <ajaykver9@gmail.com>
2023-12-31 03:17:16 +0530

    swittch

---
+++ switch.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+uisng namespace std;
+
+int main(){
+
+// Alternative of long if else statement 
+
+// case is for match 
+// break if for stop 
+// default is for else 
+
+// NOTE switch can be only integral Value , not decimal and fractional value 
+
+
+    return 0;
+}
