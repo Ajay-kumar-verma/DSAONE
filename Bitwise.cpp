eaa94a317ca502714548f729eaf5f6b7d230019e
Ajay kumar verma <ajaykver9@gmail.com>
2023-12-31 01:58:36 +0530

    Bitwise operator

---
+++ Bitwise.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ int x=2;
+ int y=3;
+
+ cout<<(x | y)<<endl;
+ cout<<(x ^ y)<<endl;
+ cout<<(~y)<<endl;
+ cout<<(x & y)<<endl;
+ cout<<endl;
+ cout<<(x>>1)<<endl;  // divided by 2
+ cout<<(y<<2)<<endl;  // multiply by 4 
+
+
+    return 0;
+}
--- Operator.cpp
+++ Operator.cpp
@@ -8,7 +8,7 @@ int main(){
 // 2 Logic  ||, & 
 // 3 comparision ,>,<,>=,<=
 // 4 Assigning +=,-=,*=,/=,%=,= 
-// 5 biwise | ,& 
+// 5 biwise | (OR) ,&(AND) , ~ (NOT) ,^ (XOR), >>(RIght SHIFT) , <<(LEFT SHIFTING)
 
 
 
